c7c61a12bcbf1fd5da149d9fb3d9874c6d31ec5c
P-h-r-e-a-k <matt_a_nice_guy_2000@yahoo.co.uk>
2020-07-27 17:41:52 +0100

    Initial commit

---
+++ HUD.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace LBAHUD
+{
+    partial class HUD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HUD));
+            this.lblKeysVal = new System.Windows.Forms.Label();
+            this.lblKashesVal = new System.Windows.Forms.Label();
+            this.sbHealth = new DisplayBar.statusBar();
+            this.pbHealth = new System.Windows.Forms.PictureBox();
+            this.pbMagic = new System.Windows.Forms.PictureBox();
+            this.sbMagic = new DisplayBar.statusBar();
+            this.pbKash = new System.Windows.Forms.PictureBox();
+            this.pbKey = new System.Windows.Forms.PictureBox();
+            this.pbZlitos = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHealth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMagic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKash)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKey)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbZlitos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblKeysVal
+            // 
+            this.lblKeysVal.AutoSize = true;
+            this.lblKeysVal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKeysVal.Location = new System.Drawing.Point(401, 2);
+            this.lblKeysVal.Name = "lblKeysVal";
+            this.lblKeysVal.Size = new System.Drawing.Size(17, 17);
+            this.lblKeysVal.TabIndex = 7;
+            this.lblKeysVal.Text = "2";
+            // 
+            // lblKashesVal
+            // 
+            this.lblKashesVal.AutoSize = true;
+            this.lblKashesVal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKashesVal.Location = new System.Drawing.Point(314, 2);
+            this.lblKashesVal.Name = "lblKashesVal";
+            this.lblKashesVal.Size = new System.Drawing.Size(35, 17);
+            this.lblKashesVal.TabIndex = 9;
+            this.lblKashesVal.Text = "999";
+            // 
+            // sbHealth
+            // 
+            this.sbHealth.BackColor = System.Drawing.Color.Transparent;
+            this.sbHealth.ColourEmpty = System.Drawing.Color.LightGray;
+            this.sbHealth.ColourFont = System.Drawing.Color.Black;
+            this.sbHealth.ColourFull = System.Drawing.Color.PaleVioletRed;
+            this.sbHealth.DisplayValue = false;
+            this.sbHealth.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sbHealth.Location = new System.Drawing.Point(29, 0);
+            this.sbHealth.Max = ((uint)(100u));
+            this.sbHealth.Min = ((uint)(0u));
+            this.sbHealth.Name = "sbHealth";
+            this.sbHealth.Size = new System.Drawing.Size(112, 20);
+            this.sbHealth.TabIndex = 13;
+            this.sbHealth.Val = ((uint)(50u));
+            // 
+            // pbHealth
+            // 
+            this.pbHealth.Image = ((System.Drawing.Image)(resources.GetObject("pbHealth.Image")));
+            this.pbHealth.InitialImage = ((System.Drawing.Image)(resources.GetObject("pbHealth.InitialImage")));
+            this.pbHealth.Location = new System.Drawing.Point(0, 0);
+            this.pbHealth.Name = "pbHealth";
+            this.pbHealth.Size = new System.Drawing.Size(26, 20);
+            this.pbHealth.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbHealth.TabIndex = 14;
+            this.pbHealth.TabStop = false;
+            this.pbHealth.WaitOnLoad = true;
+            this.pbHealth.Click += new System.EventHandler(this.pbHealth_Click);
+            // 
+            // pbMagic
+            // 
+            this.pbMagic.Image = ((System.Drawing.Image)(resources.GetObject("pbMagic.Image")));
+            this.pbMagic.InitialImage = ((System.Drawing.Image)(resources.GetObject("pbMagic.InitialImage")));
+            this.pbMagic.Location = new System.Drawing.Point(149, 0);
+            this.pbMagic.Name = "pbMagic";
+            this.pbMagic.Size = new System.Drawing.Size(15, 20);
+            this.pbMagic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMagic.TabIndex = 15;
+            this.pbMagic.TabStop = false;
+            this.pbMagic.Click += new System.EventHandler(this.pbMagic_Click);
+            // 
+            // sbMagic
+            // 
+            this.sbMagic.BackColor = System.Drawing.Color.Transparent;
+            this.sbMagic.ColourEmpty = System.Drawing.Color.LightGray;
+            this.sbMagic.ColourFont = System.Drawing.Color.Black;
+            this.sbMagic.ColourFull = System.Drawing.Color.CornflowerBlue;
+            this.sbMagic.DisplayValue = false;
+            this.sbMagic.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sbMagic.Location = new System.Drawing.Point(167, 0);
+            this.sbMagic.Max = ((uint)(80u));
+            this.sbMagic.Min = ((uint)(0u));
+            this.sbMagic.Name = "sbMagic";
+            this.sbMagic.Size = new System.Drawing.Size(112, 20);
+            this.sbMagic.TabIndex = 16;
+            this.sbMagic.Val = ((uint)(70u));
+            this.sbMagic.Load += new System.EventHandler(this.sbMagic_Load);
+            // 
+            // pbKash
+            // 
+            this.pbKash.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbKash.Image = ((System.Drawing.Image)(resources.GetObject("pbKash.Image")));
+            this.pbKash.Location = new System.Drawing.Point(285, 0);
+            this.pbKash.Name = "pbKash";
+            this.pbKash.Size = new System.Drawing.Size(24, 20);
+            this.pbKash.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbKash.TabIndex = 27;
+            this.pbKash.TabStop = false;
+            // 
+            // pbKey
+            // 
+            this.pbKey.Image = ((System.Drawing.Image)(resources.GetObject("pbKey.Image")));
+            this.pbKey.Location = new System.Drawing.Point(355, 0);
+            this.pbKey.Name = "pbKey";
+            this.pbKey.Size = new System.Drawing.Size(40, 20);
+            this.pbKey.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbKey.TabIndex = 28;
+            this.pbKey.TabStop = false;
+            // 
+            // pbZlitos
+            // 
+            this.pbZlitos.Image = ((System.Drawing.Image)(resources.GetObject("pbZlitos.Image")));
+            this.pbZlitos.Location = new System.Drawing.Point(285, 0);
+            this.pbZlitos.Name = "pbZlitos";
+            this.pbZlitos.Size = new System.Drawing.Size(24, 20);
+            this.pbZlitos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbZlitos.TabIndex = 56;
+            this.pbZlitos.TabStop = false;
+            // 
+            // HUD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.Controls.Add(this.pbZlitos);
+            this.Controls.Add(this.pbKey);
+            this.Controls.Add(this.pbKash);
+            this.Controls.Add(this.sbMagic);
+            this.Controls.Add(this.pbMagic);
+            this.Controls.Add(this.pbHealth);
+            this.Controls.Add(this.sbHealth);
+            this.Controls.Add(this.lblKashesVal);
+            this.Controls.Add(this.lblKeysVal);
+            this.Name = "HUD";
+            this.Size = new System.Drawing.Size(638, 20);
+            ((System.ComponentModel.ISupportInitialize)(this.pbHealth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMagic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKash)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKey)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbZlitos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblKeysVal;
+        private System.Windows.Forms.Label lblKashesVal;
+        private DisplayBar.statusBar sbHealth;
+        private System.Windows.Forms.PictureBox pbHealth;
+        private System.Windows.Forms.PictureBox pbMagic;
+        private DisplayBar.statusBar sbMagic;
+        private System.Windows.Forms.PictureBox pbKash;
+        private System.Windows.Forms.PictureBox pbKey;
+        private System.Windows.Forms.PictureBox pbZlitos;
+    }
+}
---
+++ HUD.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LBAMemoryModule;
+using System.Drawing.Text;
+using System.Reflection;
+using System.IO;
+
+namespace LBAHUD
+{
+    public partial class HUD : UserControl
+    {
+        private byte health;
+        private byte magicLevel;
+        private byte magicPoints;
+        private byte keys;
+        private byte cloverBoxes;
+        private byte clovers;
+        private int kashes;
+        private int zilitos;
+        private int magicMaxWidth = 112;
+        oTimerGetItems otgi = new oTimerGetItems();
+        private byte Health
+        {
+            get => health;
+            set
+            {
+                health = value;
+                sbHealth.Val = health;
+            }
+        }
+        private byte MagicLevel
+        {
+            get => magicLevel;
+            set
+            {
+                magicLevel = value;
+                sbMagic.Width = (int)(magicMaxWidth * ((float)magicLevel / 4));
+                sbMagic.Max = (uint)20 * magicLevel;
+            }
+        }
+        private byte MagicPoints
+        {
+            get => magicPoints;
+            set
+            {
+                magicPoints = value;
+                sbMagic.Val = magicPoints;
+            }
+        }
+        private byte Keys
+        {
+            get => keys;
+            set
+            {
+                keys = value;
+                lblKeysVal.Text = keys.ToString();
+            }
+        }
+        private PictureBox[] pbCloverBoxes = new PictureBox[10];
+        Image fullCloverBox;
+        Image emptyCloverBox;
+        Mem m = new Mem();
+        private byte CloverBoxes
+        {
+            get => cloverBoxes;
+            set
+            {
+                cloverBoxes = value;
+                UpdateClovers();
+            }
+        }
+        private byte Clovers
+        {
+            get => clovers;
+            set
+            {
+                clovers = value;
+                UpdateClovers();
+            }
+        }
+        private int Kashes
+        {
+            get => kashes;
+            set
+            {
+                kashes = value;
+                lblKashesVal.Text = value.ToString();
+            }
+        }
+        public HUD()
+        {
+            InitializeComponent();
+            //this.TransparencyKey = this.BackColor;
+        }
+        private int Zilitos
+        {
+            get => zilitos;
+            set
+            {
+                zilitos = value;
+                lblKashesVal.Text = value.ToString();
+            }
+        }
+        private string getFilesPath()
+        {
+            //return AppDomain.CurrentDomain.BaseDirectory;
+            return new Uri(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase)).LocalPath;
+        }
+        private enum LBA1Offsets : uint
+        {
+            MagicLevel = 0xE20,
+            MagicPoints = 0xE22,
+            Kashes = 0xE24,
+            Keys = 0xE26,
+            Clovers = 0xE2A,
+            CloverBoxes = 0xE2C,
+            Gas = 0xE30,
+            Health = 0xD554
+        }
+        private enum LBA2Offsets : uint
+        {
+            Health = 0x57F29,
+            Zilitos = 0x57DFB,
+            CloverBoxes = 0x3D691,
+            Clovers = 0x57DE9,
+            Kashes = 0x57DF7,
+            MagicLevel = 0x57DFF,
+            MagicPoints = 0x57E00,
+            Keys = 0x57E01
+        }
+
+        public void Refresh()
+        {
+            byte LBAVer = m.DetectLBAVersion();
+            otgi = new oTimerGetItems();
+            if (1 == LBAVer)
+            {
+                sbHealth.Max = 50;
+                otgi.AddItem(UpdateHealth, (uint)LBA1Offsets.Health, 1);
+                otgi.AddItem(UpdateMagicLevel, (uint)LBA1Offsets.MagicLevel, 1);
+                otgi.AddItem(UpdateMagicPoints, (uint)LBA1Offsets.MagicPoints, 1);
+                otgi.AddItem(UpdateKeys, (uint)LBA1Offsets.Keys, 1);
+                otgi.AddItem(UpdateCloverBoxes, (uint)LBA1Offsets.CloverBoxes, 1);
+                otgi.AddItem(UpdateClovers, (uint)LBA1Offsets.Clovers, 1);
+                otgi.AddItem(UpdateKashes, (uint)LBA1Offsets.Kashes, 2);
+                return;
+            }
+            if(2 == LBAVer)
+            {
+                sbHealth.Max = 255;
+                otgi.AddItem(UpdateHealth, (uint)LBA2Offsets.Health, 1);
+                otgi.AddItem(UpdateMagicLevel, (uint)LBA2Offsets.MagicLevel, 1);
+                otgi.AddItem(UpdateMagicPoints, (uint)LBA2Offsets.MagicPoints, 1);
+                otgi.AddItem(UpdateKeys, (uint)LBA2Offsets.Keys, 1);
+                otgi.AddItem(UpdateCloverBoxes, (uint)LBA2Offsets.CloverBoxes, 1);
+                otgi.AddItem(UpdateClovers, (uint)LBA2Offsets.Clovers, 1);
+                otgi.AddItem(UpdateKashes, (uint)LBA2Offsets.Kashes, 2);
+                otgi.AddItem(UpdateZilitos, (uint)LBA2Offsets.Zilitos, 2);
+                return;
+            }
+        }
+        public void UpdateHealth(ushort val)
+        {
+            if (val != health)
+                Health = (byte)val;
+        }
+        public void UpdateMagicLevel(ushort val)
+        {
+            MagicLevel = (byte)val;
+        }
+        public void UpdateMagicPoints(ushort val)
+        {
+            if (val != magicPoints)
+                MagicPoints = (byte)val;
+        }
+        public void UpdateKeys(ushort val)
+        {
+            Keys = (byte)val;
+        }
+        public void UpdateCloverBoxes(ushort val)
+        {
+            CloverBoxes = (byte)val;
+        }
+        public void UpdateClovers(ushort val)
+        {
+            Clovers = (byte)val;
+        }
+        public void UpdateKashes(ushort val)
+        {
+            if (isTwinsun())
+            {
+                pbKash.Visible = true;
+                pbZlitos.Visible = false;
+                kashes = val;
+                lblKashesVal.Text = kashes.ToString();
+            }
+        }
+        public void UpdateZilitos(ushort val)
+        {
+            if (!isTwinsun())
+            {
+                pbKash.Visible = false;
+                pbZlitos.Visible = true;
+                zilitos = val;
+                lblKashesVal.Text = zilitos.ToString();
+            }
+        }
+
+        private bool isTwinsun()
+        {
+            int islandOffset = -0x1C4EDD;
+            byte LBAVer = m.DetectLBAVersion();
+            if (1 == LBAVer) return true;
+            int val = m.readAddress(2,(uint)islandOffset, 1);            
+            if (0 == val || 2 == val) return true;
+            return false;
+        }
+        private void initialCloverBoxLoad(int cloverCount, int cloverBoxCount)
+        {
+            int pictureBoxXPos = 429;
+            int pictureBoxYPos = 0;
+            int pictureBoxPadding = 1;
+            int pictureBoxWidth = 20;
+            Size boxSize = new Size(pictureBoxWidth, 20);
+
+            for (int i = 0; i < pbCloverBoxes.Length; i++)
+            {
+                pbCloverBoxes[i] = new PictureBox();
+                pbCloverBoxes[i].Location = new Point(pictureBoxXPos + (i * pictureBoxWidth) + (i * pictureBoxPadding), pictureBoxYPos);
+                if (i < cloverCount)
+                    pbCloverBoxes[i].Image = fullCloverBox;
+                else
+                    pbCloverBoxes[i].Image = emptyCloverBox;
+                pbCloverBoxes[i].Size = boxSize;
+                pbCloverBoxes[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                pbCloverBoxes[i].Visible = false;
+                Controls.Add(pbCloverBoxes[i]);
+            }
+
+            for (int i = 0; i < pbCloverBoxes.Length && i < cloverBoxCount; i++)
+            {
+                pbCloverBoxes[i].Visible = true;
+            }
+        }
+
+        private void UpdateClovers()
+        {
+            for (int i = 0; i < pbCloverBoxes.Length && i < cloverBoxes; i++)
+            {
+                if (i < clovers)
+                    pbCloverBoxes[i].Image = fullCloverBox;
+                else
+                    pbCloverBoxes[i].Image = emptyCloverBox;
+                pbCloverBoxes[i].Visible = true;
+            }
+            for (int i = cloverBoxes; i < pbCloverBoxes.Length; i++)
+                pbCloverBoxes[i].Visible = false;
+        }
+
+        private void sbMagic_Load(object sender, EventArgs e)
+        {
+            //fullCloverBox = Image.FromFile("./files/fullCloverBox.png");
+            fullCloverBox = Image.FromFile(getFilesPath() + "/files/fullCloverBox.png");
+            emptyCloverBox = Image.FromFile(getFilesPath() + "/files/emptyCloverBox.png");
+            initialCloverBoxLoad(1, 2);
+            Refresh();
+        }
+
+        private void pbHealth_Click(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void pbMagic_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
